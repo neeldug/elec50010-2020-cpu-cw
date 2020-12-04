a3142797c3faafe77e081bc87742c25277d15200
bd519 <bd519@imperial.ac.uk>
2020-12-04 12:05:45 +0000

    revision using github veriloglint replacing <= by = in always case

--- rtl/mips_cpu.v
+++ rtl/mips_cpu.v
@@ -73,47 +73,47 @@ always @(*)
 		6'b000000: case(funct)									//link in reg not $31
 						6'b001001: begin 						//Jump and link register
 									controls <= 11'b10000000100;
-									jump1 <= 1;
+									jump1 = 1;
 								end
 						6'b001000: begin						//Jump register
-									controls <= 11'b00100000110; 
-									jump1 <= 1;
+									controls = 11'b00100000110; 
+									jump1 = 1;
 								end
-						6'b010001: controls <= 11'b10000000010; //Move to high       No need to write enable register?
-						6'b010100: controls <= 11'b10000000010; //Move to low		  As HI and LO are reg in ALU module
-						default: controls <= 11'b10100000010; //R-type instruction
+						6'b010001: controls = 11'b10000000010; //Move to high       No need to write enable register?
+						6'b010100: controls = 11'b10000000010; //Move to low		  As HI and LO are reg in ALU module
+						default: controls = 11'b10100000010; //R-type instruction
 					endcase
-		6'b100000: controls <= 11'b10010001000; //Load byte
-		6'b100100: controls <= 11'b10010001000; //Load byte unsigned
-		6'b100001: controls <= 11'b10010001000; //Load halfword
-		6'b100101: controls <= 11'b10010001000; //Load halfword unisigned
-		6'b001111: controls <= 11'b10010001000; //Load upper immidiate
-		6'b100011: controls <= 11'b10010001000; //Load word
-		6'b100010: controls <= 11'b10010001000; //Load word left
-		6'b100110: controls <= 11'b10010001000; //Load word right
-		6'b101000: controls <= 11'b00010100000; //Store byte
-		6'b101001: controls <= 11'b00010100000; //Store halfword
-		6'b101011: controls <= 11'b00010100000; //Store word
-		6'b000100: controls <= 11'b00001000001; //Branch on = 0
+		6'b100000: controls = 11'b10010001000; //Load byte
+		6'b100100: controls = 11'b10010001000; //Load byte unsigned
+		6'b100001: controls = 11'b10010001000; //Load halfword
+		6'b100101: controls = 11'b10010001000; //Load halfword unisigned
+		6'b001111: controls = 11'b10010001000; //Load upper immidiate
+		6'b100011: controls = 11'b10010001000; //Load word
+		6'b100010: controls = 11'b10010001000; //Load word left
+		6'b100110: controls = 11'b10010001000; //Load word right
+		6'b101000: controls = 11'b00010100000; //Store byte
+		6'b101001: controls = 11'b00010100000; //Store halfword
+		6'b101011: controls = 11'b00010100000; //Store word
+		6'b000100: controls = 11'b00001000001; //Branch on = 0
 		6'b000001: case(dest)
-						5'b00001: controls <= 11'b00001000001; //Branch on >= 0
-						5'b10001: controls <= 11'b11001010001; //Branch on >= 0 /link (regwrite active)
-						5'b00000: controls <= 11'b00001000001; //Branch on < 0
-						5'b10000: controls <= 11'b11001010001; //Branch on < 0 /link
-						default:  controls <= 11'bxxxxxxxxxxx;
+						5'b00001: controls = 11'b00001000001; //Branch on >= 0
+						5'b10001: controls = 11'b11001010001; //Branch on >= 0 /link (regwrite active)
+						5'b00000: controls = 11'b00001000001; //Branch on < 0
+						5'b10000: controls = 11'b11001010001; //Branch on < 0 /link
+						default:  controls = 11'bxxxxxxxxxxx;
 					endcase
-		6'b000111: controls <= 11'b00001000001; //Branch on > 0
-		6'b000110: controls <= 11'b00001000001; //Branch on <= 0
-		6'b000101: controls <= 11'b00001000001; //Branch on != 0
-		6'b001001: controls <= 11'b10010000010; //ADD unsigned immediate
-		6'b000010: controls <= 11'b00000000100; //Jump
-		6'b000011: controls <= 11'b11000010100; //Jump and link
-		6'b001100: controls <= 11'b10010000010; //ANDI
-		6'b001101: controls <= 11'b10010000010; //ORI
-		6'b001110: controls <= 11'b10010000010; //XORI
-		6'b001010: controls <= 11'b10010000010; //Set on less than immediate (signed)
-		6'b001011: controls <= 11'b10010000010; //Set on less than immediate unsigned
-		default:   controls <= 11'bxxxxxxxxxxx; //???
+		6'b000111: controls = 11'b00001000001; //Branch on > 0
+		6'b000110: controls = 11'b00001000001; //Branch on = 0
+		6'b000101: controls = 11'b00001000001; //Branch on != 0
+		6'b001001: controls = 11'b10010000010; //ADD unsigned immediate
+		6'b000010: controls = 11'b00000000100; //Jump
+		6'b000011: controls = 11'b11000010100; //Jump and link
+		6'b001100: controls = 11'b10010000010; //ANDI
+		6'b001101: controls = 11'b10010000010; //ORI
+		6'b001110: controls = 11'b10010000010; //XORI
+		6'b001010: controls = 11'b10010000010; //Set on less than immediate (signed)
+		6'b001011: controls = 11'b10010000010; //Set on less than immediate unsigned
+		default:   controls = 11'bxxxxxxxxxxx; //???
 	endcase
 endmodule
 
@@ -133,66 +133,66 @@ module aludec(
 always @(*)
 	case(aluop)							//edge what if we have a 2'b11 eventhough it is illegal
 	
-		2'b00: alucontrol <= 5'b00011; //ADD -- USED FOR LOAD AND STORE INSTRUCTIONS
-		2'b01: alucontrol <= 5'b00100; //SUB 
+		2'b00: alucontrol = 5'b00011; //ADD -- USED FOR LOAD AND STORE INSTRUCTIONS
+		2'b01: alucontrol = 5'b00100; //SUB 
 		2'b10: case(op)
-/*			6'b100000: alucontrol <= 5'b; //Load byte				
-			6'b100100: alucontrol <= 5'b; //Load byte unsigned
-			6'b100001: alucontrol <= 5'b; //Load halfword			
-			6'b100101: alucontrol <= 5'b; //Load halfword unsigned
-			6'b001111: alucontrol <= 5'b; //Load upper immidiate
-			6'b100011: alucontrol <= 5'b; //Load word
-			6'b100010: alucontrol <= 5'b; //Load word left         
-			6'b100110: alucontrol <= 5'b; //Load word right
-			6'b101000: alucontrol <= 5'b; //Store byte
-			6'b101001: alucontrol <= 5'b; //Store halfword
-			6'b101011: alucontrol <= 5'b; //Store word
+/*			6'b100000: alucontrol = 5'b; //Load byte				
+			6'b100100: alucontrol = 5'b; //Load byte unsigned
+			6'b100001: alucontrol = 5'b; //Load halfword			
+			6'b100101: alucontrol = 5'b; //Load halfword unsigned
+			6'b001111: alucontrol = 5'b; //Load upper immidiate
+			6'b100011: alucontrol = 5'b; //Load word
+			6'b100010: alucontrol = 5'b; //Load word left         
+			6'b100110: alucontrol = 5'b; //Load word right
+			6'b101000: alucontrol = 5'b; //Store byte
+			6'b101001: alucontrol = 5'b; //Store halfword
+			6'b101011: alucontrol = 5'b; //Store word
 */			
-			6'b000100: alucontrol <= 5'b00100; //Branch on = 0 use SUBU
+			6'b000100: alucontrol = 5'b00100; //Branch on = 0 use SUBU
 			6'b000001: case(dest)
-						5'b00001: alucontrol <= 5'b00110; //Branch on >= 0 use SLT
-						5'b10001: alucontrol <= 5'b00110; //Branch on >= 0 /link (regwrite active) use SLT mod in control sign
-						5'b00000: alucontrol <= 5'b10011; //Branch on < 0 
-						5'b10000: alucontrol <= 5'b10100; //Branch on < 0 /link
-						default: alucontrol <= 5'bxxxxx;
+						5'b00001: alucontrol = 5'b00110; //Branch on >= 0 use SLT
+						5'b10001: alucontrol = 5'b00110; //Branch on >= 0 /link (regwrite active) use SLT mod in control sign
+						5'b00000: alucontrol = 5'b10011; //Branch on < 0 
+						5'b10000: alucontrol = 5'b10100; //Branch on < 0 /link
+						default: alucontrol = 5'bxxxxx;
 					endcase
-			6'b000111: alucontrol <= 5'b10101; //Branch on > 0
-			6'b000110: alucontrol <= 5'b10110; //Branch on <= 0
-			6'b000101: alucontrol <= 5'b10111; //Branch on != 0
-			6'b001001: alucontrol <= 5'b00011; //ADD unsigned immediate
-			6'b000010: alucontrol <= 5'b; //Jump
-			6'b000011: alucontrol <= 5'b; //Jump and link
-			6'b001100: alucontrol <= 5'b00000; //ANDI
-			6'b001101: alucontrol <= 5'b00001; //ORI
-			6'b001110: alucontrol <= 5'b00010; //XORI
-			6'b001010: alucontrol <= 5'b00110; //Set on less than immediate (signed)
-			6'b001011: alucontrol <= 5'b00101; //Set on less than immediate unsigned
+			6'b000111: alucontrol = 5'b10101; //Branch on > 0
+			6'b000110: alucontrol = 5'b10110; //Branch on = 0
+			6'b000101: alucontrol = 5'b10111; //Branch on != 0
+			6'b001001: alucontrol = 5'b00011; //ADD unsigned immediate
+			6'b000010: alucontrol = 5'b; //Jump
+			6'b000011: alucontrol = 5'b; //Jump and link
+			6'b001100: alucontrol = 5'b00000; //ANDI
+			6'b001101: alucontrol = 5'b00001; //ORI
+			6'b001110: alucontrol = 5'b00010; //XORI
+			6'b001010: alucontrol = 5'b00110; //Set on less than immediate (signed)
+			6'b001011: alucontrol = 5'b00101; //Set on less than immediate unsigned
 			
 			6'b000000: case(funct)
-							6'b100001: alucontrol <= 5'b00011; //ADD -> ADDU
-							6'b100100: alucontrol <= 5'b00000; //AND -> AND
-							6'b100011: alucontrol <= 5'b00100; //SUB unsigned -> SUBU
-							6'b100101: alucontrol <= 5'b00001; //bitwise OR -> OR
-							6'b100110: alucontrol <= 5'b00010; //bitwise XOR -> XOR
-							6'b101010: alucontrol <= 5'b00110; //SLT -> SLT
-							6'b101011: alucontrol <= 5'b00101; //SLTUnsigned -> SLTU
-							6'b011001: alucontrol <= 5'b00111; //Multiply unsigned -> MULTU
-							6'b011000: alucontrol <= 5'b01000; //Multiply -> MULT
-							6'b000000: alucontrol <= 5'b01001; //Shift left logical ->SLL
-							6'b000100: alucontrol <= 5'b01010; //Shift left logical variable -> SLLV
-							6'b000011: alucontrol <= 5'b01011; //Shift right arithmetic -> SRA
-							6'b000010: alucontrol <= 5'b01100; //Shift right logical -> SRL
-							6'b000111: alucontrol <= 5'b01101; //Shift right arithmetic variable -> SRAV
-							6'b000110: alucontrol <= 5'b01110; //Shift right logical variable -> SRLV
-							6'b011010: alucontrol <= 5'b01111; //Divide signed DIV
-							6'b011011: alucontrol <= 5'b10000; //Divide unsigned DIVU
-							6'b010001: alucontrol <= 5'b10001; //MTHI
-							6'b010100: alucontrol <= 5'b10010; //MTLO
-							6'b001000: alucontrol <= 5'b11001; //Jump register JR
-							6'b001001: alucontrol <= 5'b00000; //Jump and link register
-							default:   alucontrol <= 5'bxxxxx; //???
+							6'b100001: alucontrol = 5'b00011; //ADD -> ADDU
+							6'b100100: alucontrol = 5'b00000; //AND -> AND
+							6'b100011: alucontrol = 5'b00100; //SUB unsigned -> SUBU
+							6'b100101: alucontrol = 5'b00001; //bitwise OR -> OR
+							6'b100110: alucontrol = 5'b00010; //bitwise XOR -> XOR
+							6'b101010: alucontrol = 5'b00110; //SLT -> SLT
+							6'b101011: alucontrol = 5'b00101; //SLTUnsigned -> SLTU
+							6'b011001: alucontrol = 5'b00111; //Multiply unsigned -> MULTU
+							6'b011000: alucontrol = 5'b01000; //Multiply -> MULT
+							6'b000000: alucontrol = 5'b01001; //Shift left logical ->SLL
+							6'b000100: alucontrol = 5'b01010; //Shift left logical variable -> SLLV
+							6'b000011: alucontrol = 5'b01011; //Shift right arithmetic -> SRA
+							6'b000010: alucontrol = 5'b01100; //Shift right logical -> SRL
+							6'b000111: alucontrol = 5'b01101; //Shift right arithmetic variable -> SRAV
+							6'b000110: alucontrol = 5'b01110; //Shift right logical variable -> SRLV
+							6'b011010: alucontrol = 5'b01111; //Divide signed DIV
+							6'b011011: alucontrol = 5'b10000; //Divide unsigned DIVU
+							6'b010001: alucontrol = 5'b10001; //MTHI
+							6'b010100: alucontrol = 5'b10010; //MTLO
+							6'b001000: alucontrol = 5'b11001; //Jump register JR
+							6'b001001: alucontrol = 5'b00000; //Jump and link register
+							default:   alucontrol = 5'bxxxxx; //???
 						endcase
-			default: alucontrol <= 5'bxxxxx; //???		
+			default: alucontrol = 5'bxxxxx; //???		
 			endcase	
 	endcase
 endmodule
