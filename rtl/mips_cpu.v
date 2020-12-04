a0bdddd79c4862b2ccb2b2f596295a4be6f07ac5
bd519 <bd519@imperial.ac.uk>
2020-12-04 11:41:02 +0000

    revision using github veriloglint

--- rtl/mips_cpu.v
+++ rtl/mips_cpu.v
@@ -39,7 +39,7 @@ logic branch;
 
 maindec md(op, funct, dest, memtoreg2, memtoreg1, data_write, branch, alusrc, regdst2, regdst1, regwrite, jump1, jump, aluop);
 
-aludec ad(funct, op, aluop, alucontrol);
+aludec ad(funct, op, dest, aluop, alucontrol);
 	always @(*) begin
 		assign pcsrc = branch & zero;
 	end
@@ -126,6 +126,7 @@ endmodule
 
 module aludec(
 	input logic [5:0] funct,op,
+	input logic [4:0] dest,
 	input logic [1:0] aluop,
 	output logic [4:0] alucontrol);
 
