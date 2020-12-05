5e327d517a94946ea4367f3e5c9fb11a7b4b3cd9
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-05 21:02:07 +0000

    refactor: general cleanup
    - removes unnecessary scripts
    - changes makefile to store in correct
    directories

--- test/assembly/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$0")
-
-mips-linux-gnu-gcc -S -EB -mfp32 -O3 -march=mips1 "$1" -o "$BASEDIR""/"$(basename "$1" .c).asm.txt
--- test/generate_all_c_files.sh
+++ test/generate_all_c_files.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
-
-for file in "$BASEDIR"/c-files/*.c; do
-  ./"$BASEDIR"/assembly/generate.sh "$file"
-done
+# Generates all binaries from c-files, execute from test directory
 
 cd c-files
 for file in *.c; do
   TESTNAME=$(basename "$file" | cut -f 1 -d '.')
   make "$TESTNAME".hex.txt
 done
-
-mv *.hex.txt ../binary/
