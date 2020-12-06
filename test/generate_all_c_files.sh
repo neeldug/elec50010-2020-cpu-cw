d85635f06b522141de66f5fc111626ea163d7601
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-06 15:36:30 +0000

    add asm generation to script

--- test/generate_all_c_files.sh
+++ test/generate_all_c_files.sh
@@ -2,8 +2,9 @@
 
 # Generates all binaries from c-files, execute from test directory
 
-cd c-files
+cd c-files || return 1
 for file in *.c; do
   TESTNAME=$(basename "$file" | cut -f 1 -d '.')
+  make "$TESTNAME".asm.txt
   make "$TESTNAME".hex.txt
 done
