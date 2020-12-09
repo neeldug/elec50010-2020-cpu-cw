e148e68be351cf322c3bf8cbb62019326e897962
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-09 15:38:18 +0000

    style(testbench): lints and formats for ease of debug/readability

--- test/testbench-verilog/data_mem.v
+++ test/testbench-verilog/data_mem.v
@@ -1,32 +1,32 @@
-module data_mem(
-	input logic clk,
-	input logic[31:0] data_address,
-	input logic[31:0] data_writedata,
-	input logic data_write,
-	output logic[31:0] data_readdata
+module data_mem (
+    input logic clk,
+    input logic [31:0] data_address,
+    input logic [31:0] data_writedata,
+    input logic data_write,
+    output logic [31:0] data_readdata
 );
 
-	parameter DATA_INIT_FILE = "";
+  parameter str DATA_INIT_FILE = "";
 
-	reg [31:0] dmem [4294967295:0];
+  reg [31:0] dmem[4294967295:0];
 
-	//data initialization
-	initial begin
-		integer i;
-		for (i=0; i<4294967296; i++) begin
-			dmem[i] = 0;
-		end
+  //data initialization
+  initial begin
+    integer i;
+    for (i = 0; i < 4294967296; i++) begin
+      dmem[i] = 0;
+    end
 
-		if(DATA_INIT_FILE != "") begin
-			$readmemh(DATA_INIT_FILE, imem);
-		end
-	end
+    if (DATA_INIT_FILE != "") begin
+      $readmemh(DATA_INIT_FILE, imem);
+    end
+  end
 
-	//clockedge read and write with write control signal
-	always @(posedge clk) begin
-		if(data_write) begin
-			dmem[data_address] <= data_writedata;
-		end
-		data_readdata <= dmem[data_address];
-	end
+  //clockedge read and write with write control signal
+  always @(posedge clk) begin
+    if (data_write) begin
+      dmem[data_address] <= data_writedata;
+    end
+    data_readdata <= dmem[data_address];
+  end
 endmodule
--- test/testbench-verilog/tb_harvard.v
+++ test/testbench-verilog/tb_harvard.v
@@ -1,77 +1,94 @@
 module tb_harvard;
 
-    parameter INSTR_INIT_FILE =  /*default test case if not specified*/;
-    parameter DATA_INIT_FILE = /*default data if not specified*/;
-    parameter TIMEOUT_CYCLES = 10000;
+  parameter str INSTR_INIT_FILE = ""  /*default test case if not specified*/;
+  parameter str DATA_INIT_FILE = ""  /*default data if not specified*/;
+  parameter unsigned TIMEOUT_CYCLES = 10000;
 
-    timeunit 1ns / 10ps;
+  timeunit 1ns / 10ps;
 
-    logic clk;
-    logic reset;
-    logic active;
-    logic [31:0] register_v0;
+  logic clk;
+  logic reset;
+  logic active;
+  logic [31:0] register_v0;
 
-    logic clk_enable; //Needs investigating further - logic to drive signal?
+  logic clk_enable;  //Needs investigating further - logic to drive signal?
 
-    logic[31:0] instr_address;
-    logic[31:0] instr_readdata;
+  logic [31:0] instr_address;
+  logic [31:0] instr_readdata;
 
-    logic[31:0] data_address;
-    logic data_write;
-    logic data_read;
-    logic[31:0] data_writedata;
-    logic[31:0] data_readdata;
+  logic [31:0] data_address;
+  logic data_write;
+  logic data_read;
+  logic [31:0] data_writedata;
+  logic [31:0] data_readdata;
 
-    instruction_mem #(INSTR_INIT_FILE) instRAMInst(instr_address, instr_readdata);
-    data_mem #(DATA_INIT_FILE) dataRAMInst(clk, data_address, data_writedata, data_write, data_readdata);
+  instruction_mem #(INSTR_INIT_FILE) instRAMInst (
+      .instr_address(instr_address),
+      .instr(instr_readdata)
+  );
+  data_mem #(DATA_INIT_FILE) dataRAMInst (
+      .clk(clk),
+      .data_address(data_address),
+      .data_writedata(data_writedata),
+      .data_write(data_write),
+      .data_readdata(data_readdata)
+  );
 
-    mips_cpu_harvard cpuInst(clk, reset, active, register_v0, clk_enable, instr_address, instr_readdata, data_address, data_write, data_read, 				      data_writedata, data_readdata);
+  mips_cpu_harvard cpuInst (
+      .clk(clk),
+      .reset(reset),
+      .active(active),
+      .register_v0(register_v0),
+      .clk_enable(clk_enable),
+      .instr_address(instr_address),
+      .instr_readdata(instr_readdata),
+      .data_address(data_address),
+      .data_write(data_write),
+      .data_read(data_read),
+      .data_writedata(data_writedata),
+      .data_readdata(data_readdata)
+  );
 
-    //Setting up a clock
-    initial begin
-	clk = 0;
+  //Setting up a clock
+  initial begin
+    clk = 0;
 
-	repeat (TIMEOUT_CYCLES) begin
-	    #10;
-	    clk = !clk;
-	    #10;
-	    clk = !clk;
-	end
-
-	$fatal(1, "Simulation didn't finish within %d cycles.", TIMEOUT_CYCLES);
-
+    repeat (TIMEOUT_CYCLES) begin
+      #10;
+      clk = !clk;
+      #10;
+      clk = !clk;
     end
 
-    initial begin
-	//Resetting the CPU
-	reset <= 0;
+    $fatal(1, "Simulation didn't finish within %d cycles.", TIMEOUT_CYCLES);
 
-	@(posedge clk);
-	reset <= 1;
+  end
 
-	@(posedge clk);
-	reset <= 0;
+  initial begin
+    //Resetting the CPU
+    reset <= 0;
 
-	@(posedge clk);
-	//Checking reset was successful
-	assert(active==1) else $display("Testbench: CPU didn't set active high after reset");
+    @(posedge clk);
+    reset <= 1;
 
-	//Looping until the CPU finished (sets active low) - add clk_enable logic here
-	while(active) begin
-	    @(posedge clk);
-	end
+    @(posedge clk);
+    reset <= 0;
 
-	$display("Testbench: Finished. Active = 0");
-	$display("Final value of register v0 = %d", register_v0);
+    @(posedge clk);
+    //Checking reset was successful
+    assert (active == 1)
+    else $display("Testbench: CPU didn't set active high after reset");
 
-	$finish;
-
+    //Looping until the CPU finished (sets active low) - add clk_enable logic here
+    while (active) begin
+      @(posedge clk);
     end
+
+    $display("Testbench: Finished. Active = 0");
+    $display("Final value of register v0 = %d", register_v0);
+
+    $finish;
+
+  end
 
 endmodule
-
-		
-	
-	
-
-
