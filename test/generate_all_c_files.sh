03fcb80450f67e443ace5600744f01a6a1cdaa7e
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-11 19:24:26 +0000

    improve generator performance
    only generate if file not exist

--- test/generate_all_c_files.sh
+++ test/generate_all_c_files.sh
@@ -5,15 +5,17 @@
 cd c-files || return 1
 for file in *.c; do
   TESTNAME=$(basename "$file" | cut -f 1 -d '.')
-  make "$TESTNAME".asm.txt
-  make "$TESTNAME".hex.txt
-  make "$TESTNAME".ref.txt
-  make "$TESTNAME".hex.data.txt
-done
-
-cd .. || return 1
-
-# Trim final line
-for file in data/*.txt; do
-  sed -i '$d' "$file"
+  if [ ! -f  ../assembly/"$TESTNAME".asm.txt ]; then
+    make "$TESTNAME".asm.txt
+  fi
+  if [ ! -f ../binary/"$TESTNAME".hex.txt ]; then
+    make "$TESTNAME".hex.txt
+  fi
+  if [ ! -f ../reference/"$TESTNAME".ref.txt ]; then
+    make "$TESTNAME".ref.txt
+  fi
+  if [ ! -f ../data/"$TESTNAME".hex.data.txt ]; then
+    make "$TESTNAME".hex.data.txt && sed -i '$d' ../data/"$TESTNAME".hex.data.txt
+    # Trim final line
+  fi
 done
