074584f95db8e0eeb8664d69b69f667323e2d164
tlp19 <tlp19@imperial.ac.uk>
2020-12-16 20:39:48 +0000

    adapted CPU to little endian memory

--- rtl/cpu_design_harvard/mips_cpu_harvard.v
+++ rtl/cpu_design_harvard/mips_cpu_harvard.v
@@ -21,6 +21,78 @@ module mips_cpu_harvard (
   
   logic [4:0] alucontrol;
   logic [2:0] loadcontrol;
+  
+  
+  
+  // SWITCH FROM LITTLE ENDIAN TO BIG ENDIAN AND VICE-VERSA WHEN READING/WRITING TO MEMORIES
+  logic [31:0] 	instr_readdata_be, 
+  				data_readdata_be, 
+  				data_writedata_be;
+  
+  endian_switch switch_instr_readdata (
+  	  .in(instr_readdata),
+      .out(instr_readdata_be)
+  );
+  
+  endian_switch switch_data_readdata (
+  	  .in(data_readdata),
+      .out(data_readdata_be)
+  );
+  
+  endian_switch switch_data_writedata (
+  	  .in(data_writedata_be),
+      .out(data_writedata)
+  );
+
+
+controller control (
+      .op(instr_readdata_be[31:26]),
+      .funct(instr_readdata_be[5:0]),
+      .dest(instr_readdata_be[20:16]),
+      .zero(zero),
+      .memtoreg2(memtoreg2),
+      .memtoreg1(memtoreg1),
+      .data_write(data_write),
+      .pcsrc(pcsrc),
+      .alusrc(alusrc),
+      .regdst2(regdst2),
+      .regdst1(regdst1),
+      .regwrite(regwrite),
+      .jump1(jump1),
+      .jump(jump),
+      .alucontrol(alucontrol),
+      .loadcontrol(loadcontrol)
+  );
+
+  datapath datap(
+      .clk(clk),
+      .reset(reset),
+      .clk_enable(clk_enable),
+      .memtoreg2(memtoreg2),
+      .memtoreg1(memtoreg1),
+      .alusrc(alusrc),
+      .pcsrc(pcsrc),
+      .regdst2(regdst2),
+      .regdst1(regdst1),
+      .regwrite(regwrite),
+      .jump1(jump1),
+      .jump(jump),
+      .alucontrol(alucontrol),
+      .loadcontrol(loadcontrol),
+      .zero(zero),
+      .instr_address(instr_address),
+      .instr_readdata(instr_readdata_be),
+      .data_readdata(data_readdata_be),
+      .data_address(data_address),
+      .data_writedata(data_writedata_be),
+      .register_v0(register_v0),
+      .register_v3(register_v3),
+      .pcsrclast(pcsrclast)
+  );
+
+
+
+/* OLD VERSION WITHOUT ENDIAN SWITCH
 
   controller control (
       .op(instr_readdata[31:26]),
@@ -67,6 +139,8 @@ module mips_cpu_harvard (
       .pcsrclast(pcsrclast)
   );
   
+*/
+
   	
 /*
  assign active = (instr_address==0) ? 0 : 1; //If PC counter points to address 0, then the active flag is set to 0.
