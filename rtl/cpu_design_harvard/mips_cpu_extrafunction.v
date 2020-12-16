ace252b45c91a43c2092cb122fdad6507a48d231
tlp19 <tlp19@imperial.ac.uk>
2020-12-16 18:35:14 +0000

    added endian_switch module

--- rtl/cpu_design_harvard/mips_cpu_extrafunction.v
+++ rtl/cpu_design_harvard/mips_cpu_extrafunction.v
@@ -152,3 +152,13 @@ module signext (
 
   assign y = {{16{a[15]}}, a};
 endmodule
+
+
+module endian_switch (
+	input logic [31:0] in,
+	output logic [31:0] out,
+);
+
+	assign out = {in[7:0], in[15:8], in[23:16], in[31:24]};
+	
+endmodule
