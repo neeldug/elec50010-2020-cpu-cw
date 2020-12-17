b368678f197797484dcf5ca00a18b30caa1ea22e
bd519 <bd519@imperial.ac.uk>
2020-12-17 10:26:16 +0000

    done a first draft of the finite state machine of the bus cpu

--- rtl/cpu_design_bus/mips_cpu_controller_maindec.v
+++ rtl/cpu_design_bus/mips_cpu_controller_maindec.v
@@ -1,53 +1,201 @@
 module maindec (
+	input logic clk,
+	reset,
     input logic [5:0] op,
     funct,
     input logic [4:0] dest,
     output logic memtoreg1,
-    data_write,
+    memwrite,
     output logic branch,
-    alusrc,
+    alusrca,
     output logic regdst2,
     regdst1,
     output logic regwrite,
     output logic jump1,
     jump,
+    output logic iord, 
+    irwrite, 
+    pcwrite
+    output logic [1:0] alusrcb,
     output logic [1:0] aluop,
+    output logic [2:0] state,
     output logic [2:0] loadcontrol
 );
 
 
-  reg [9:0] controls;
+/*	State:
+	000 : Fetch
+	001 : Decode
+	010 : S2
+	011 : S3
+	100 : S4
+*/
 
-
-  //Probably needs to use a always@(*)
-
-  assign {regwrite, regdst2, regdst1, alusrc, branch, data_write, memtoreg1, jump, aluop} = controls;
-
-
-  // Assign 11 elements names as aluop consist of 2 bits so rightfully fills the reg controls.
-  // Correspond to the bits below from left to right in the same order (starting with regwrite and ending with aluop).
-
-always @(posedge clk)begin //Stage 1: Fetch.
-	iord = 0;
-	alusrca = 0;
-	alusrcb = 2'b01;
-	aluop = 2'b00;
-	pcsrc = 0;
-	irwrite = 1;
-	pcwrite = 1;
-	always @(posedge clk) begin //Stage 2: buffer.
-		// do nothing to let the memory output and the register store.
-		always @(posedge clk) begin
-	
-
-
-			end
-		end
+always @(posedge clk) begin
+	if (reset) begin
+		state <= 000;
+	end 
+	else if (state == 000) begin	//Fetch state: PC incrementation
+		iord <= 0;
+		alusrca <= 0;
+		alusrcb <= 01;
+		aluop <= 00;
+		pcsrc <= 00;		//to check
+		irwrite <= 1;
+		pcwrite <= 1;
+		state <= 001;
 	end
-endmodule
-
-
-// We are currently setting all the control signals by looking at the opcode of the instrunctions.
-// We created an reg (=array) of control signals so that it is easier to implement.
-// In order to understand this section please refer to page 376 of the book (table 7.3) for team.
-
+	else if (state == 001) begin	//Decode instruction: Branch instr
+		alusrca <= 0;
+		alusrcb <= 11;
+		aluop <= 00;
+		state <= 010;
+	end
+	else if (state == 010) begin	//S2 state: ALU operations
+		case (op)
+			6'b000000:  		//R-type instructions
+				case (funct)
+					6'b010001 | 6'b010100: begin	//MTHI MTLO
+						alusrca <= 1;
+						alusrcb <= 00;
+						aluop <= 10;
+						state <= 000;
+					end
+					6'b001001: begin	//Jump register
+						aluop <= 01;
+						pcsrc <= 10;
+						pcwrite <= 1;
+						state <= 000;
+						end
+					6'b001001: begin	//Jump register and link
+						aluop <= 01;
+						pcsrc <= 10;
+						pcwrite <= 1;
+						state <= 011;
+						end
+					default: begin	//R-type instruction basic
+						alusrca <= 1;
+						alusrcb <= 00;
+						aluop <= 10;
+						state <= 011;
+					end
+				endcase
+			6'b000100 | 6'b000111 | 6'b000110 | 6'b000101: begin	//Branches
+				alusrca <= 1;
+				alusrcb <= 00;
+				aluop <= 10;
+				pcsrc <= 01;
+				branch <= 1;
+				state <= 000;
+			end
+			6'b0000001:	//branches
+				case (dest)
+				 5'b10001 | 5'b10000: begin	//Branch with link
+					alusrca <= 1;
+					alusrcb <= 00;
+					aluop <= 10;
+					pcsrc <= 01;
+					branch <= 1;
+					state <= 011;
+				 end
+				 default: begin
+				 	alusrca <= 1;
+					alusrcb <= 00;
+					aluop <= 10;
+					pcsrc <= 01;
+					branch <= 1;
+					state <= 000;
+				 end
+				endcase
+			6'b001001 | 6'b001100 | 6'b001101 | 6'b001110 | 6'b001010 | 6'b001011 | 6'b001111: begin	//R-type with immediate & LUI at end.
+				alusrca <= 1;
+				alusrcb <= 10;
+				aluop <= 10;
+				state <= 011;
+				end
+			6'b000010: begin	//Jump
+				aluop <= 01;
+				pcsrc <= 10;
+				pcwrite <= 1;
+				state <= 000;
+				end
+			6'b000011: begin	//Jump and Link
+				aluop <= 01;
+				pcsrc <= 10;
+				pcwrite <= 1;
+				state <= 011;
+				end
+			6'b100000 | 6'b100100 | 6'b100001 | 6'b100101 | 6'b100011 | 6'b100010 | 6'b100110 | 6'b101000 | 6'b101001 | 6'b101011: begin //store and load instructions except for LUI
+				alusrca <= 1,
+				alusrcb <= 10;
+				aluop <= 00;
+				state <= 011;
+				end
+			default: begin		//error to check
+				state <= 000;
+				active <= 0;
+				end
+		endcase
+	end
+	else if (state == 011) begin
+		case (op)
+			6'b100000 | 6'b100100 | 6'b100001 | 6'b100101 | 6'b100011 | 6'b100010 | 6'b100110: begin	//Load instructions
+				iord <= 1;
+				state <= 100;
+				end
+			6'b101000 | 6'b101001 | 6'b101011: begin	//Store instructions
+				iord <= 1;
+				memwrite <= 1;
+				state <= 000;
+				end
+			6'b000000:  		//R-type instructions
+				case (funct)
+					6'b001001: begin	//Jump register and link
+						regdst2 <= 1;
+						memtoreg <= 0;
+						regwrite <= 1;
+						state <= 000;
+						end
+					default: begin	//R-type instruction basic.
+						regdst1 <= 1;
+						regdst2 <= 0
+						memtoreg <= 0;
+						regwrite <= 1;
+						state <= 000;
+					end
+				endcase
+			6'b000001: begin	//branch and link.
+				regdst2 <= 1;
+				memtoreg <= 0;
+				regwrite <= 1;
+				state <= 000;
+				end
+			6'b001001 | 6'b001100 | 6'b001101 | 6'b001110 | 6'b001010 | 6'b001011 | 6'b001111: begin	//R-type with immediate & LUI at end.
+				regdst1 <= 1;
+				regdst2 <= 0
+				memtoreg <= 0;
+				regwrite <= 1;
+				state <= 000;
+				end
+			default: begin	//error to check.
+				state <= 000;
+				active <= 0;
+				end
+		endcase
+	end
+	else if (state == 100) begin
+		regdst <= 0;
+		regdst2 <= 0;
+		memtoreg <= 1;
+		regwrite <= 1;
+		state <= 000;
+	end
+	else if (active == 0) begin
+		// do nothing.
+	end
+	else begin	//error state.
+		state <= 000;
+		active <= 0;
+	end
+end 
+endmodule		 
--- rtl/cpu_design_bus/mips_cpu_datapath.v
+++ rtl/cpu_design_bus/mips_cpu_datapath.v
@@ -1,68 +1,278 @@
-module datapath(
-	input logic clk, reset,
-	input logic memtoreg2, memtoreg1,
-	input logic alusrc, pcsrc,
-	input logic regdst2, regdst1,
-	input logic regwrite,
-	input logic jump1, jump,
-	
-	input logic iord, irwrite,
-	input logic alusrca,
-	input logic [1:0] alusrcb,
-	
-	
-	input logic [4:0] alucontrol,
-	input logic [2:0] loadcontrol,
-	output logic zero,
-	output logic [31:0] address,
-	input logic [31:0] readdata,
-	output logic [31:0] writedata,
-	output logic [31:0] register_v0);
+module datapath (
+    input logic clk,
+    reset,
+//    clk_enable,
+    input logic memtoreg2,
+    memtoreg1,
+    input logic alusrc,
+    pcsrc,
+    input logic regdst2,
+    regdst1,
+    input logic regwrite,
+    input logic jump1,
+    jump,
+    input logic [4:0] alucontrol,
+    input logic [2:0] loadcontrol,
+    output logic zero,
+    output logic [31:0] address,
+    input logic [31:0] readdata,
+    output logic [31:0] writedata,
+    output logic [31:0] register_v0, register_v3,
+    output logic pcsrclast
+);
 
-logic [31:0] pcout;
-logic [31:0] instr, data;
-logic [31:0] rd1, rd2, rda, srca, srcb;
-logic [31:0] signimm;
-logic [31:0] aluresult, aluout;
+//to add in definition of module's entities.
 
-//PC register:
-flipflopr #(32) pcregist(.clk(clk), .reset(reset), .enable(1), .d(aluresult), .q(pcout));
+  input logic [1:0] alusrcb,
+  input logic alusrca,
+  input logic irwrite, iord,
+  logic [31:0] pcnext, pcout, instr, data, aluresult, aluout;
+  logic [31:0] rd1, rd2, rega, regb;
+  
+//end 
 
-//selector Instruction/Data:
-mux2 #(32) (.a(pcout), .b(aluout), .s(iord), .y(address));
+  logic [4:0] writereg1, writereg;
+  logic [31:0] pcnext, pcnextbr, pcrst, pcplus4, pcbranch, pclink, pcnextbrin, pcnextbrout;
+  logic [31:0] signimm, signimmsh, pcnextbr1, pcnextbr2, jumpsh;
+  logic [31:0] srca, srcb;
+  logic [31:0] result2, result1, result;
+  logic [31:0] pcresult;
+//  logic pcsrclast;
+  
+/*
+  regfile1 #(31) jumpbrmem (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(memtoreg2),
+	  .d(pcnextbrin),
+	  .q(pcnextbrout)
+  ); 
+  regfile2 #(0) pcsrcreg (
+  	  .clk(clk),
+  	  .reset(reset),
+  	  .we(1'b1),	//change control signal as now takes more than 1 clk cycle maybe @(writereg).
+  	  .d(pcsrc),
+  	  .q(pcsrclast)
+  );
+*/
+ 
+/*  mux2 #(32) pcmux (
+  	  .a(pcnextbr1),
+      .b(pcplus4),
+      .s(memtoreg2), 
+      .y(pcnextbr)
+  );
+  
+  flipflopr #(32) pcreg (
+      .clk(clk),
+      .reset(reset),
+      .clk_enable(clk_enable),
+      .d(pcnextbr),
+      .q(instr_address)
+  );
+*/
 
-//Memory register:
-flipflop #(32) IR(.clk(clk), .reset(reset), .enable(irwrite), .d(readdata), .q(instr));
+// Program counter regfile
 
-flipflop #(32) DATAreg(.clk(clk), .reset(reset), .enable(1), .d(readdata), .q(data));
+/*
 
+  mux2 #(32) pcmux1 (
+      .a({6'b0, instr[25:0]}),
+      .b(pcnext),
+      .s(jump1),
+      .y(pcnextbr2)
+  );  //jump1 is high when we jump to value in register.
 
-//Register file block:
-regfile register(.clk(clk), .reset(reset), .we3(regwrite), .ra1(instr[25:21]), .ra2(), .wa3(instr[20:16]), .wd3(data), .rd1(rd1), .rd2(rd2), .reg_v0(register_v0));
-
-flipflop #(32) rdastore(.clk(clk), .reset(reset), .enable(1), .d(rd1), .q(rda));
-
-signext se(instr[15:0], signimm);
-
-
-//ALU module:
-alu alumodule(.reset(reset), .control(alucontrol), .a(srca), .b(srcb), .shamt(instr[10:6]), .zero(zero), .y(aluresult)); 
-
-mux2 #(32) srcAsel(.a(pcout), .b(rda), .s(alusrca), .y(srca));
-mux4 #(32) srcBsel(.a(), .b(3'b100), .c(signimm), .d(), .s(alusrcb), .y(srcb));
-
-flipflop #(32) alureg(.clk(clk), .reset(reset), .enable(1), .d(aluresult), .q(aluout));
+  jumpsl2 jsh (
+      .a(pcnextbr2),
+      .control(jump),
+      .y(pcnextbrin)
+  );  //Instruction in PC are every 4 so we need to multiply by 4 when it is jump.
 
 
+  mux2 #(32) pcmux2bis (
+      .a(pcbranch),
+      .b(jumpsh),
+      .s(jump), //jump
+      .y(pcnextbrin)
+  );  //jump is high when we are in a jump instruction.
+
+  mux2 #(32) pcmux2 (
+      .a(pcnext),
+      .b(pcnextbrout),
+      .s(pcsrclast), //pcsrc
+      .y(pcnextbr1)
+  );  //pcsrc is high when we are in a branch instruction and the condition. (zero flag) are met.
+
+  mux2 #(32) pcmux (
+  	  .a(pcnextbr1),
+      .b(pcplus4),
+      .s(memtoreg2), 
+      .y(pcnextbr)
+  );
+*/ 
+  
+    
+  mux2 #(32) PCsrc (
+  	  .a(aluresult),
+  	  .b(aluout),
+  	  .s(pcsrc),
+  	  .y(pcnext)
+  );
+  
+  
+  regfile1 #(31) pcreg (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(pcen),
+	  .d(pcnext),
+	  .q(pcout)
+  );
+  
+
+  mux2 #(32) addrsel(
+	  .a(pcout),
+	  .b(aluout),
+	  .s(iord),
+	  .y(address)
+  );
+  
+// INstruction and Data registers
+  regfile1 #(31) ir (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(irwrite),
+	  .d(readdata),
+	  .q(instr)
+  );
+  
+  regfile1 #(31) datareg (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(1'b1),
+	  .d(readdata),
+	  .q(data)
+  );
+
+// Register module  
+  signext se (
+      .a(instr[15:0]),
+      .y(signimm)
+  );
+
+  shiftleft2 immshift (  //used for branch implementation.
+      .a(signimm),
+      .y(signimmsh)
+  );
+
+
+
+  // Register file
+
+  //we added an output: register_v0 so that this value is accessible from the outside of the Mips_cpu at all time.
+  regfile register (
+      .clk(clk),
+      .reset(reset),
+      .we3(regwrite),
+      .ra1(instr[25:21]),
+      .ra2(instr[20:16]),
+      .wa3(writereg),
+      .wd3(result),
+      .rd1(rd1),
+      .rd2(rd2),
+      .reg_v0(register_v0),
+      .reg_v3(register_v3)
+  );
+  
+  regfile1 #(31) regdst1 (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(1'b1),
+	  .d(rd1),
+	  .q(rega)
+  );
+  
+  regfile1 #(31) regdst2 (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(1'b1),
+	  .d(rd2),
+	  .q(regb)
+  );
+  
+  mux2 #(5) wrmux (
+      .a(instr[20:16]),
+      .b(instr[15:11]),
+      .s(regdst1),
+      .y(writereg1)
+  );  //regdst1 is high for R-type instructions else select I-type.
+  
+  mux2 #(5) wrmux2 (
+      .a(writereg1),
+      .b(5'b11111),
+      .s(regdst2),
+      .y(writereg)
+  );  //regdst2 is high for link instructions (store value in $31).
+
+
+  loadselector loadsel (
+      .a(data), //data_readadta
+      .controls(loadcontrol),
+      .y(result1)
+  );
+
+  //Result--value written in register (Load/Branches/Jump)
+  mux2 #(32) resmux (
+      aluout,
+      result1,
+      memtoreg1,
+      result
+  );  //memtoreg1 is high for load instructions (value in RAM) else take result from ALU.
+
+/*  mux2 #(32) resmux2 (
+      result2,
+      pclink,
+      memtoreg2,
+      result
+  );  //memtoreg2 is high for Branch with condition met and Jump with link instructions.  */ 
+  
+// We won't need that as we can use another adder and will have to use ALU.
 
 
 
 
+// ALU file input selectors:
+  mux4 #(32) srcbmux (
+      .a(regb),			//register value		00
+      .b(32'b100),  	//PC + 4				01
+      .c(signimm),  	//Immediate variable 	10
+      .d(signimmsh),	//Branch instr			11
+      .s(alusrcb),
+      .y(srcb)
+  ); 
+  
+  mux2 #(32) srcamux (
+  	  .a(pcout),		//PC for Branch			0
+  	  .b(rega),			//register value		1
+  	  .s(alusrca),
+  	  .y(srca)
+  );
 
-
-
-
-
-
-
-
+  alu alumodule (
+      .reset(reset),
+      .control(alucontrol),
+      .a(srca),
+      .b(srcb),
+      .shamt(instr[10:6]),
+      .zero(zero),
+      .y(aluresult)
+  );
+  
+  regfile1 #(31) alureg (
+	  .clk(clk),
+	  .reset(reset),
+	  .we(1'b1),
+	  .d(aluresult),
+	  .q(aluout)
+  );
+endmodule
