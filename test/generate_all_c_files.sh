0d8629dd0b56b28c7fa15207ae2c86bf527f417a
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-19 16:20:54 +0000

    minor formatting changes and parallelise gen

--- test/generate_all_c_files.sh
+++ test/generate_all_c_files.sh
@@ -2,8 +2,8 @@
 
 # Generates all binaries from c-files, execute from test directory
 
-cd c-files || return 1
-for file in *.c; do
+c_gen() {
+  local file=$1
   TESTNAME=$(basename "$file" | cut -f 1 -d '.')
   if [ ! -f  ../assembly/"$TESTNAME".asm.txt ]; then
     make "$TESTNAME".asm.txt
@@ -17,21 +17,33 @@ for file in *.c; do
   if [ ! -f ../data/"$TESTNAME".hex.data.txt ]; then
     make "$TESTNAME".hex.data.txt
   fi
+}
+
+cd c-files || return 1
+for file in *.c; do
+  c_gen "$file" &
 done
+
+asm_gen() {
+  local file=$1
+  TESTNAME=$(basename "$file" | cut -f 1 -d '.')
+  if [ ! -f  ../assembly/"$TESTNAME".asm.txt ]; then
+    make "$TESTNAME".asm.txt
+  fi
+  if [ ! -f ../binary/"$TESTNAME".hex.txt ]; then
+    make "$TESTNAME".hex.txt
+  fi
+  if [ ! -f ../data/"$TESTNAME".hex.data.txt ]; then
+    make "$TESTNAME".hex.data.txt
+  fi
+  if [ ! -f ../reference/"$TESTNAME".ref.txt ]; then
+    make "$TESTNAME".ref.txt
+  fi
+}
 
 cd ../raw-assembly || return 1
 for file in *.s; do
-  TESTNAME=$(basename "$file" | cut -f 1 -d '.')
-  if [ ! -f  ../assembly/"$TESTNAME".asm.txt ]; then
-    make "$TESTNAME".asm.txt
-  fi
-  if [ ! -f ../binary/"$TESTNAME".hex.txt ]; then
-    make "$TESTNAME".hex.txt
-  fi
-  if [ ! -f ../data/"$TESTNAME".hex.data.txt ]; then
-    make "$TESTNAME".hex.data.txt
-  fi
-  if [ ! -f ../reference/"$TESTNAME".ref.txt ]; then
-    make "$TESTNAME".ref.txt
-  fi
+  asm_gen "$file" &
 done
+
+wait
