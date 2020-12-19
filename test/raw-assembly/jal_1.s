909c61ce3c3397ff18376ca0d07754add10466aa
thepelin <pu19@ic.ac.uk>
2020-12-19 19:18:08 +0300

    revised jal_1

--- test/raw-assembly/jal_1.s
+++ test/raw-assembly/jal_1.s
@@ -9,14 +9,13 @@ main:
 	MOVE $2, $0
 	MOVE $4, $31
 	JAL pass
-	nop
 	ADDU $2,$2,5
+	MOVE $31, $4
 	JR $31
 	.end main
-    .set	noreorder
-    .set	nomacro
+    	.set	noreorder
+    	.set	nomacro
 
 pass:
 	ADDU $2,$2,3
-	MOVE $31, $4
 	JR $31
