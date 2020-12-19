8de25615c88b59b06e84d94460f298ec16d31cbd
root <root@DESKTOP-5TN4RA6.localdomain>
2020-12-19 16:10:17 +0000

    bltzal linking

--- test/raw-assembly/bltzal_1.s
+++ test/raw-assembly/bltzal_1.s
@@ -9,12 +9,15 @@ main:
 	MOVE $2, $0
 	LI $3, 0xFFFFFFFF
 	MOVE $4, $31
-	BLTZAL $3, main + 28
-	nop
-	ADDU $2,$2,5
-	ADDU $2,$2,3
+	BLTZAL $3, label
 	MOVE $31, $4
+	ADDU $2,$2,3
 	JR $31
 	.end main
     .set	noreorder
     .set	nomacro
+
+
+label:
+	ADDU $2,$2,4
+	JR $31
