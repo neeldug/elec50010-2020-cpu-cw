d877c6bfc31256c130ef42b18ba1a6e22575b097
thl19git <59955474+thl19git@users.noreply.github.com>
2020-12-20 19:35:20 +0000

    Update mips_cpu_extrafunction.v

--- rtl/cpu_design_bus/mips_cpu_extrafunction.v
+++ rtl/cpu_design_bus/mips_cpu_extrafunction.v
@@ -9,7 +9,8 @@ module regfile (
     input logic [31:0] wd3,
     output logic [31:0] rd1,
     rd2,
-    reg_v0, reg_v3
+    reg_v0//
+	//reg_debug											//debug (from datapath)
 );
 
   reg [31:0] rf[31:0];
@@ -32,54 +33,65 @@ module regfile (
 
   assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
   assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
-  assign reg_v0 = (~reset) ? rf[2] : reg_v0;		//NOT SURE ABOUT THIS, MIGHT HAVE TO USE AN ALWAYS BLOCK
-  assign reg_v3 = (~reset) ? rf[3] : reg_v3;
+  assign reg_v0 = (~reset) ? rf[2] : 0;
+  //assign reg_debug = (~reset) ? rf[31] : reg_debug;		//debug (from datapath)
 endmodule
 
 
 module regfile2 #(
-    parameter WIDTH = 31)(
+    parameter WIDTH = 31
+) (
     input logic clk,
     reset,
     input logic we,
     input logic [WIDTH:0] d,
     output logic [WIDTH:0] q
 );
-reg i;
+  reg i;
 
   always_ff @(posedge clk) begin
     if (reset) begin  //sets all the regs in the regfile to 0 is reset signal is high
-    	q <= 0;
-    	i <= 1;
+      q <= 0;
+      i <= 1;
     end else begin
-      if (we) begin 
-      		q <= i ? 0 : d;
-      		i <= 0;
-      		end
+      if (we) begin
+        q <= i ? 0 : d;
+        i <= 0;
+      end
     end
   end
 endmodule
 
 
 module regfile1 #(
-    parameter WIDTH = 31)(
+    parameter WIDTH = 32
+) (
     input logic clk,
     reset,
-    input logic we,
-    input logic [WIDTH:0] d,
-    output logic [WIDTH:0] q
+    clk_enable,
+    stall,
+    input logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
 );
 
-  always_ff @(posedge clk) begin
-    if (reset) begin  //sets all the regs in the regfile to 0 is reset signal is high
-    	q <= 0;
-    end else begin
-      if (we) q <= d;
-    end
-  end
-endmodule
 
-// Implementation of reusable functions used in datapath
+  /*always @(negedge reset) begin
+  	q = 32'hBFC00004;
+  end*/
+
+  always_ff @(posedge clk or negedge reset) begin
+	if(clk) begin
+    if (reset) begin  //sets all the regs in the regfile to 0 is reset signal is high
+      q <= 32'h00000000;
+    end else begin
+      if (clk_enable & (~stall)) q <= d;
+    end
+	end else begin
+	 	q <= 32'hBFC00004;
+	end
+  end
+  
+endmodule
 
 
 module mux2 #(
@@ -95,24 +107,6 @@ module mux2 #(
 endmodule
 
 
-module mux4 #(
-	parameter WIDTH = 8
-) (
-	input logic [WIDTH - 1:0] a,
-	b,
-	c,
-	d,
-	input logic [1:0] s,
-	output logic [WIDTH - 1:0] y
-);
-reg [31:0] x, z;
-
-	assign x = s[1]? c : a;
-	assign z = s[1]? d : b;
-	assign y = s[0]? z : x;
-endmodule	
-	
-
 module adder (
     input  logic [31:0] a,
     b,
@@ -122,6 +116,7 @@ module adder (
   assign y = a + b;
 endmodule
 
+
 module shiftleft2 (
     input  logic [31:0] a,
     output logic [31:0] y
@@ -130,15 +125,6 @@ module shiftleft2 (
   assign y = {{a[29:0]}, 2'b00};
 endmodule
 
-module jumpsl2 (
-    input  logic [31:0] a,
-    input logic control,
-    output logic [31:0] y
-);
-	if(control) begin
-		assign y = {{a[29:0]}, 2'b00};
-	end else y = a; 
-endmodule
 
 module shiftleft16 (
     input  logic [31:0] a,
@@ -148,32 +134,6 @@ module shiftleft16 (
   assign y = {{a[15:0]}, 16'b0};
 endmodule
 
-module flipflopr #(
-    parameter WIDTH = 32
-) (
-    input logic clk,
-    reset,
-    clk_enable,
-    input logic [WIDTH-1:0] d,
-    output logic [WIDTH-1:0] q
-);
- reg x;
-	
-	initial
-		x = 1'b0;
-	always @(negedge reset) begin
-    	x = 1'b1;
-    end
-  
-  always_ff @(posedge clk) begin
-    if (reset) q <= x ? 32'hBFC00000 : 32'b0;
-    else if (clk_enable) begin
-    	q <= x ? 32'hBFC00000 : d;
-    	x <= 0;
-  	end
-  end
-
-endmodule
 
 module signext (
     input  logic [15:0] a,
@@ -184,11 +144,36 @@ module signext (
 endmodule
 
 
-module endian_switch (
-	input logic [31:0] in,
-	output logic [31:0] out
+module flipflopr #(
+    parameter WIDTH = 32
+) (
+    input logic clk,
+    reset,
+    clk_enable,
+    stall,
+    output logic active,
+    input logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
 );
+  
+  /*always @(negedge reset) begin
+    active = 1'b1;
+    q = 32'hBFC00000;
+  end*/
 
-	assign out = {in[7:0], in[15:8], in[23:16], in[31:24]};
-	
+  always_ff @(posedge clk or negedge reset) begin
+	if(clk) begin
+    if (reset) begin
+      q <= 32'b0;
+    end
+    else if (clk_enable & (~stall)) begin
+      if (d==32'b0) active <= 0;
+      q <= d;
+    end
+	end else begin
+	 active = 1'b1;
+    q = 32'hBFC00000;
+	end
+  end
+
 endmodule
