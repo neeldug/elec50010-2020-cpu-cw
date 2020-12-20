168fad8772282c6f40639ba56d45e5f7b0cf5d80
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-20 00:07:13 +0000

    more cases for lb and lbu

---
+++ test/c-files/lb_5.c
@@ -0,0 +1,7 @@
+signed char x = 5;
+signed char y = 3;
+signed char z = -3;
+signed char a = -7;
+int main(void) {
+    return y;
+}
---
+++ test/c-files/lbu_3.c
@@ -0,0 +1,8 @@
+unsigned char a = 5;
+unsigned char b = 3;
+unsigned char c = 7;
+unsigned char d = 9;
+
+int main(void) {
+    return b;
+}
