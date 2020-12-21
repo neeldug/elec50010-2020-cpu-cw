5619468f52f630a1ff91895055967c40722f44b4
nd419 <5161147+neeldug@users.noreply.github.com>
2020-12-21 16:26:11 +0000

    more branch testcases

---
+++ test/c-files/blez_3.c
@@ -0,0 +1,8 @@
+int b = -4;
+
+int main(void) {
+    if (b <= 0) {
+        return 3;
+    }
+    return 4;
+}
---
+++ test/c-files/bne_2.c
@@ -0,0 +1,8 @@
+int j = 3;
+
+int main(void) {
+    if (j != 3) {
+        return 7;
+    }
+    return 2;
+}
